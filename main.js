b27e690254616f8e78a6470293d356319cfb6b92
Jeremygim2002 <127067985+Jeremygim2002@users.noreply.github.com>
2023-03-25 11:56:45 -0500

    crear una app de pregunta trampa

---
+++ main.js
@@ -0,0 +1,24 @@
+let si = document.querySelector('#btn_si');
+
+si.addEventListener('click', ()=>{
+    Swal.fire({
+        title: 'Sabia que dirias que si :3',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      })
+})
+
+let no = document.querySelector('#btn_no');
+
+no.addEventListener('mouseover', ()=>{
+    let randomX = parseInt(Math.random()*100);
+    let randomY = parseInt(Math.random()*100);
+    no.style.setProperty('top',randomY+'%');
+    no.style.setProperty('left',randomX+'%');
+    no.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
+
